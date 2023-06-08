a3f1f4c229ee5816ff08692d7902a77b18bc50b0
jeffball <jeffball@jeffballs-mbp.lan>
2023-06-08 06:47:24 -0700

    First commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "minschedule": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "minscheduledb3ceb11": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { Authenticator } from '@aws-amplify/ui-vue';
+  import '@aws-amplify/ui-vue/styles.css';
+  import ToDo from './components/ToDo.vue'
+</script>
+
+<template>
+<div id="app">
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Account: {{ user.attributes.email }}</h1>
+      <button @click="signOut">Sign Out</button>
+      <ToDo></ToDo>
+    </template>
+  </authenticator>
+    </div>
+</template>
